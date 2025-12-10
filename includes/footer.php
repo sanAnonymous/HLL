3c049f491a4d5a15291fef12c7b7eedca2a95899
sanAnonymous <168445873+sanAnonymous@users.noreply.github.com>
2025-12-10 16:06:05 +0300

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,39 @@
+<footer>
+  <div class="footer-top">
+    <div class="container">
+      <div class="row">
+
+        <div class="col-md-6">
+          <h6>About Us</h6>
+          <ul>
+            <li><a href="page.php?type=aboutus">About Us</a></li>
+            <li><a href="page.php?type=privacy">Privacy Policy</a></li>
+            <li><a href="page.php?type=terms">Terms and Conditions</a></li>
+          </ul>
+        </div>
+
+         <div class="col-md-6">
+          <h6>Contact US</h6>
+          <ul>
+            <li><a href="https://facebook.com/"><i class="fa fa-facebook-square" aria-hidden="true"></i></a> Facebook</li>
+              <li><a href="https://twitter.com/"><i class="fa fa-twitter-square" aria-hidden="true"></i></a> X</li>
+          </ul>
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+  <div class="footer-bottom">
+    <div class="container">
+      <div class="row">
+        
+        <div class="col-md-12">
+          <p class="discimer">HeavyLine Logistics is not affiliated with TruckerMP, SCS Software, Euro Truck Simulator 2 or American Truck Simulator.</p>
+          <p class="copy-right">Copyright &copy; 2025 Anonymoussan69@gmail.com</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
+<!-- /Footer-->
---
+++ index.php
@@ -0,0 +1,141 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+
+<title>HeavyLine Virtual Comany</title>
+
+<!-- CSS FILES -->
+<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+<link rel="stylesheet" href="assets/css/style.css">
+<link rel="stylesheet" href="assets/css/owl.carousel.css">
+<link rel="stylesheet" href="assets/css/owl.transitions.css">
+<link rel="stylesheet" href="assets/css/bootstrap-slider.min.css">
+<link rel="stylesheet" href="assets/css/font-awesome.min.css">
+<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
+</head>
+
+<body>
+
+<!-- HEADER -->
+<?php include('includes/header.html'); ?>
+<!-- /HEADER -->
+
+<!-- BANNER -->
+<section id="banner" class="banner-section">
+  <div class="container">
+    <div class="row">
+      <div class="banner_content_full">
+        <div class="banner_content">
+          <h1>Become a HeavyLine Logistic Driver.</h1>
+          <p>
+            Ready to haul? Join HeavyLine Logistic today and start your journey as a 
+            professional truck driver. Enjoy competitive pay, flexible schedules, and 
+            a supportive community. Sign up now and hit the road with us!
+          </p>
+          
+        </div>
+      </div>
+    </div>
+  </div>
+  <button class="btn">Join Now</button>
+</section>
+
+<!-- CARS SECTION -->
+<section class="section-padding gray-bg">
+    <div class="row">
+      <div class="recent-tab">
+        <ul class="nav nav-tabs">
+          <li class="active"><a href="#varieties" data-toggle="tab">OUR COMPANY FLEET</a></li>
+        </ul>
+      </div>
+
+      <div class="tab-content">
+        <div id="varieties" class="tab-pane active">
+
+          <!-- STATIC EXAMPLE CAR 1 -->
+          <div class="col-list-3">
+            <div class="recent-car-list">
+              <div class="car-info-box">
+                <a href="#"><img src="assets/images/1.jpg" class="img-responsive" alt="Car"></a>
+                <ul>
+                  <li><i class="fa fa-car"></i> Euro Truck Simulator 2</li>
+                  <li><i class="fa fa-calendar"></i> 540 HP</li>
+                  <li><i class="fa fa-user"></i> Actros</li>
+                </ul>
+              </div>
+              <div class="car-title-m">
+                <h6>Mercedes</h6>
+              </div>
+              <div class="inventory_info_m">
+                <p>Spacious SUV ideal for long-distance travel and family trips.</p>
+              </div>
+            </div>
+          </div>
+
+          <!-- STATIC EXAMPLE CAR 2 -->
+          <div class="col-list-3">
+            <div class="recent-car-list">
+              <div class="car-info-box">
+                <a href="#"><img src="assets/images/2.jpg" class="img-responsive" alt="Car"></a>
+                <ul>
+                  <li><i class="fa fa-car"></i> Euro Truck Simulator 2</li>
+                  <li><i class="fa fa-calendar"></i> 540 HP</li>
+                  <li><i class="fa fa-user"></i> Actros</li>
+                </ul>
+              </div>
+              <div class="car-title-m">
+                <h6>Mercedes</h6>
+              </div>
+              <div class="inventory_info_m">
+                <p>Spacious SUV ideal for long-distance travel and family trips.</p>
+              </div>
+            </div>
+          </div>
+
+          <!-- STATIC EXAMPLE CAR 3 -->
+          <div class="col-list-3">
+            <div class="recent-car-list">
+              <div class="car-info-box">
+                <a href="#"><img src="assets/images/3.jpg" class="img-responsive" alt="Car"></a>
+                <ul>
+                  <li><i class="fa fa-car"></i> Euro Truck Simulator 2</li>
+                  <li><i class="fa fa-calendar"></i> 540 HP</li>
+                  <li><i class="fa fa-user"></i> Actros</li>
+                </ul>
+              </div>
+              <div class="car-title-m">
+                <h6>Mercedes</h6>
+              </div>
+              <div class="inventory_info_m">
+                <p>Spacious SUV ideal for long-distance travel and family trips.</p>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+    </div>
+  </div>
+</section>
+
+
+
+<!-- FOOTER -->
+<?php include('includes/footer.php'); ?>
+<!-- /FOOTER -->
+
+<!-- Scripts -->
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/interface.js"></script>
+<script src="assets/js/bootstrap-slider.min.js"></script>
+<script src="assets/js/slick.min.js"></script>
+<script src="assets/js/owl.carousel.min.js"></script>
+
+</body>
+</html>
